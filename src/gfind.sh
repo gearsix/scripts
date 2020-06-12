8b4f80ef7c093368781b73399fbdbded60841162
GeaRSiX <gearsix.net>
2020-06-12 15:49:37 +0100

    added dependency doc to gfind

--- src/gfind.sh
+++ src/gfind.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # gfind (grep find)
 # description: find files containing string $2 in directory/file $1
+# dependencies: grep
 # e.g.$ gfind dogs/ "shades"
 
 grep -rnw $1 -e "$2"  #recursive, print line #, match whole word
