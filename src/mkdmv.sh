b5fc7e5f01ff876e2e96f350f8ad2db0a97b1358
GeaRSiX <gearsix@tuta.io>
2020-06-23 00:04:41 +0100

    escaped filepaths in mkdmv

--- src/mkdmv.sh
+++ src/mkdmv.sh
@@ -4,13 +4,13 @@
 # e.g.$ mkdmv ~/new-dir ~/var/tmp/file*
 
 #mkdir $1
-dir=$1;
-mkdir -p $dir
+dir="$1";
+mkdir -p "$dir"
 shift;
 
 #cp files ($2...$n) into $1
 for file in $@; do
-    mv $file $dir;
+    mv "$file" "$dir";
     shift;
 done
 
