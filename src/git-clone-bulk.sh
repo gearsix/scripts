0a70b6a1b9654cb220cd4b721c83687b37475c88
GeaRSiX <gearsix@tuta.io>
2020-07-01 22:13:23 +0100

    doc note in git-clone-bulk

--- src/git-clone-bulk.sh
+++ src/git-clone-bulk.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# description: clone a set of git repos to current directory
+# description: clone a set of git repos to current directory - use sh substitution (rep{1,2,3}) for repos that share hosts
 # ARGUMENTS: git-clone-bulk repo1 repo2 repo3 ...
+# DEPENDENCIES: git
 
 for repo in $@; do git clone $repo; done
