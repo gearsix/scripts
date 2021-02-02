05d29409a92708547be79117ff458a628955d007
gearsix <gearsix@tuta.io>
2021-02-02 15:20:30 +0000

    fixed git-cfg

--- src/git-cfg.sh
+++ src/git-cfg.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
-# git-setcfg
+#!/usr/bin/env bash
+# git-cfg
 # description: set git config user.name ($1) & user.email ($2) in current dir, defaults to using --local
-# ARGUMENTS: -g, --global = "git config --global ..."
+# ARGUMENTS: -g, --global = "git config --global ..." | -h, --help = prints help
+
+if [[ $1 == "--help" || $1 == "-h" ]]; then
+	echo "Usage: git-cfg [ARGUMENTS] [NAME] [EMAIL]"
+  echo ""
+	echo "shorthand script to set the user.name and user.email values in a git"
+  echo "config."
+  echo ""
+	echo "ARGUMENTS"
+  echo "  -l, --local   use 'git config --local (default)"
+	echo "  -g, --global  use 'git config --global'"
+  exit
+fi
 
 global=0
 if [[ $1 == "--global" || $1 == "-g" ]]; then
@@ -10,9 +22,7 @@ if [[ $1 == "--global" || $1 == "-g" ]]; then
 fi
 
 username=$1
-shift
 useremail=$2
-shift
 
 if [[ $global -eq 1 ]]; then
 	git config --global user.name "$username"
