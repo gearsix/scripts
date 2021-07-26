eb3e76ad3bb53cf1c24edc9d757806a7654b001a
gearsix <gearsix@tuta.io>
2021-07-26 16:27:16 +0100

    added -r to backup

--- src/backup.sh
+++ src/backup.sh
@@ -4,11 +4,17 @@
 # e.g.$ backup ~/.bashrc ~/.vimrc
 
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-	echo "Usage: 'backup FILEPATHS...'"
+	echo "Usage: 'backup [OPTIONS] FILEPATHS...'"
 	echo ""
 	echo "copy all listed files (in FILEPATHS...) to \$filename.bak"
+	echo ""
+	echo "OPTIONS (must be provided before FILEPATHS...)"
+	echo "  -r      move files instead of copying them, removing the original"
 	exit
 fi
 
-for f in ${@}; do cp $f $f.bak; done
+backup=cp
+if [ "$1" == "-r" ]; then backup=mv; fi
+
+for f in ${@}; do $backup $f $f.bak; done
 
