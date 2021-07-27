1a9baa3ed2f84e9f1d75e7383dad26a3871a6e93
gearsix <gearsix@tuta.io>
2021-07-27 13:26:43 +0100

    shh -> shush

--- src/shush.sh
+++ src/shush.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # shh = quiet for commands that don't include it
 # description: run a terminal command ($@) and send stdout to /dev/null
-# e.g.$ shh chromium &
+# e.g.$ shush chromium &
 
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-	echo "Usage: quiet ARGS"
+	echo "Usage: shush ARGS"
 	echo ""
 	echo "run all commands in ARGS but send stdout to /dev/null, for when a tool is too noisy"
 	exit
