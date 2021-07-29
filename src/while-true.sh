6fbdb30295771706a1a4fd8e451394600a69992b
gearsix <gearsix@tuta.io>
2021-07-29 15:48:33 +0100

    bugfix in while-true

--- src/while-true.sh
+++ src/while-true.sh
@@ -7,7 +7,7 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 	echo "Usage: while-true \"ACTION\" [SLEEP]"
 	echo ""
 	echo "while-true will loop infinitely and repeat ACTION every [SLEEP] seconds."
-	echo "ACTION should be the terminal command to carry out.
+	echo "ACTION should be the terminal command to carry out."
 	echo "  Note that it needs to be provided inbetween \"\" marks so as not to be read as multiple arguments"
 	echo "SLEEP should provide the number of seconds to sleep for (default: 1)"
 	exit
@@ -15,6 +15,7 @@ fi
 
 while [ true ]; do
 	clear
+	pwd
 	$1
 	if [ $2 ]; then
 		sleep $2
