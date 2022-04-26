360f9b5e7d4c0794faf3d30848e362d07283cd76
gearsix <gearsix@tuta.io>
2022-04-26 09:50:02 +0100

    backup: fixed some shellcheck errs, changed -r to -m

--- nix/backup.sh
+++ nix/backup.sh
@@ -9,12 +9,10 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 	echo "copy all listed files (in FILEPATHS...) to \$filename.bak"
 	echo ""
 	echo "OPTIONS (must be provided before FILEPATHS...)"
-	echo "  -r      move files instead of copying them, removing the original"
+	echo "  -m      move files instead of copying them, removing the original"
 	exit
 fi
 
-backup=cp
-if [ "$1" = "-r" ]; then backup=mv; fi
-
-for f in ${@}; do $backup $f $f.bak; done
-
+alias backup='cp'
+if [ "$1" = "-m" ]; then shift; alias backup='mv'; fi
+for f in "${@}"; do backup "$f" "$f.bak"; done
